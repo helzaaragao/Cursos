8c62c61b70ecd2b2d17ec0256bd8190634899287
helzaaragao <helza.batista.aragao@gmail.com>
2024-01-26 18:03:23 -0300

    revisando conceitos

--- Rocketseat/ReactJS/Ignite Feed/src/components/Post.jsx
+++ Rocketseat/ReactJS/Ignite Feed/src/components/Post.jsx
@@ -15,16 +15,15 @@ export function Post(){
             </div>
             <time 
                title='23 de Janeiro ás 16:30h'
-               dateTime="2023-01-11 16:30:30 ">Publicado há 1h</time>
+               dateTime="2023-01-11 16:30:30">Publicado há 1h</time>
            </header>
 
            <div className={styles.content}>
-            <p>Fala galeraa 👋  </p>
-
-            <p> Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀   </p>
-
+            <p>Fala galeraa 👋 </p>
+            <p>Acabei de subir mais um projeto no meu portifa. 
+            É um projeto que fiz no NLW Return, evento da Rocketseat. 
+            O nome do projeto é DoctorCare 🚀</p>
             <p>👉{' '}<a href="">jane.design/doctorcare</a></p>
-
              <p> 
                 <a href="">#novoprojeto</a>{' '}
                 <a href="">#nlw</a>{' '}
