1d9e62dbd0ebddc5b5903fe749e4ebbc35bb2e45
helzaaragao <helza.batista.aragao@gmail.com>
2024-03-08 16:16:35 -0300

    task novas e cumpridas, colocando a funcionalidade do input e button

--- Rocketseat/ReactJS/desafio-todo/src/App.tsx
+++ Rocketseat/ReactJS/desafio-todo/src/App.tsx
@@ -1,23 +1,60 @@
-//import { useState } from 'react'
+import { useState } from 'react'
 import style from './App.module.css'
 import { Header } from '../src/components/Header'
 import { Input } from './components/Input'
 import { Button } from './components/Button'
+import {Header as ListHeader } from './components/List/Header'
 
 import {PlusCircle} from '@phosphor-icons/react';
 
+export interface ITask { 
+  id: number, 
+  text: string, 
+  isChecked: boolean
+}
+
 export function App() {
+
+  const [inputValue, setInputValue] = useState(''); 
+  const [tasks, setTasks] = useState<ITask[]>([]);
+
+  function handleAddTask(){
+     if(!inputValue){ 
+      return 
+     }
+
+     const newTask: ITask = {
+      id: new Date().getTime(), 
+      text: inputValue, 
+      isChecked: false, 
+     }
+
+     setTasks((state => [...state, newTask]))
+     setInputValue(''); 
+
+  }
 
   return (
     <>
      <Header></Header>
      
-     <section>
+     <section className={style.content}>
         <div className={style.newtask}>
-          <Input></Input>
-          <Button>Criar
-          <PlusCircle size={16} />
+          <Input 
+          onChange={(e) => setInputValue(e.target.value)}
+          value={inputValue}
+          ></Input>
+
+          <Button
+           onClick={handleAddTask}
+          > Criar <PlusCircle size={16} />
           </Button>
+        </div>
+        
+        <div className={style.taskList}>
+         <ListHeader 
+         taskCounter={0} 
+         checkedTaskCounter={0} ></ListHeader>
         </div>
 
      </section>
