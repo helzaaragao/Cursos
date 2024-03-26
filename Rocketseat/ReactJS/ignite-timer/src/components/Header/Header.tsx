dba709b20f20b536e597641e0897d282ffcca1b3
helzaaragao <helza.batista.aragao@gmail.com>
2024-03-26 15:37:24 -0300

    terminando forms e inputs

--- Rocketseat/ReactJS/ignite-timer/src/components/Header/Header.tsx
+++ Rocketseat/ReactJS/ignite-timer/src/components/Header/Header.tsx
@@ -1,7 +1,21 @@
-import { HeaderContainer } from "./styles";
+import { HeaderContainer } from './styles'
+import { Timer, Scroll } from 'phosphor-react'
 
-export function Header(){
-    return( 
-        <HeaderContainer></HeaderContainer>
-    )
+import logoIgnite from '../../assets/logo.svg'
+import { NavLink } from 'react-router-dom'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={logoIgnite} alt="" />
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24}></Timer>
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24}></Scroll>
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
 }
