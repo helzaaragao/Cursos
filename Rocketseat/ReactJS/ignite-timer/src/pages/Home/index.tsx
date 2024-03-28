7befc8e0be9e34e9e92e35a25bf62f83a98ca8e5
helzaaragao <helza.batista.aragao@gmail.com>
2024-03-28 17:24:14 -0300

    inputs e estilização do history

--- Rocketseat/ReactJS/ignite-timer/src/pages/Home/index.tsx
+++ Rocketseat/ReactJS/ignite-timer/src/pages/Home/index.tsx
@@ -8,17 +8,41 @@ import {
   StartCountdownButton,
   TaskInput,
 } from './styles.ts'
+import { useState } from 'react'
+
+// controlled (mantem em tempo real controle do que o usuario está digitando ou fazendo em uma variavel | toda vez atualiza quando utilizada e pode ser um peso ao sistema em certos momentos/ uncontrolled ( você não ver em tempo real mas fica sem atualizar tudo quando utilizada e atualiza apenas uma vez)
 
 export function Home() {
+  const [task, setTask] = useState('')
+
   return (
     <HomeContainer>
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput id="task" placeholder="Dê um nome para a sua task" />
+          <TaskInput
+            id="task"
+            placeholder="Dê um nome para a sua task"
+            list="task-suggestions"
+            onChange={(e) => setTask(e.target.value)}
+            value={task}
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1"></option>
+            <option value="Projeto 2"></option>
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
-          <MinutesAmountInput type="number" id="minutesAmount" />
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+            // utiliza para setar um min de minutos e max de minutos nesse input permitido e o step é que vai mudando de 5 em 5 exemplo: primeiro= 5 e depois 10, 15, 20
+          />
 
           <span>minutos.</span>
         </FormContainer>
