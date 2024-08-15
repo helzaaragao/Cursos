e1390701feb86eeaf3f78f0fcd47aefcd6ed1f5d
helzaaragao <helza.batista.aragao@gmail.com>
2024-08-15 12:58:31 -0300

    botões de entra e saida html,css e acessibilidade

--- Rocketseat/ReactJS/03-dt-money/src/components/NewTransactionModal/index.tsx
+++ Rocketseat/ReactJS/03-dt-money/src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import * as Dialog from '@radix-ui/react-dialog'
-import { CloseButton, Content, Overlay } from './styles'
-import { X } from 'phosphor-react'
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles'
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
+import * as RadioGroup from '@radix-ui/react-radio-group';
+
 export function NewTransactionModal(){ 
     return(
         <Dialog.Portal>
@@ -14,6 +16,17 @@ export function NewTransactionModal(){
                         <input type="text" placeholder='Descrição' required />
                         <input type="text" placeholder='Preço' required />
                         <input type="text" placeholder='Categoria' required />
+
+                        <TransactionType>
+                            <TransactionTypeButton variant="income" value='income'>
+                                <ArrowCircleUp size={24}></ArrowCircleUp>
+                                Entrada
+                            </TransactionTypeButton>
+                            <TransactionTypeButton variant="outcome" value='outcome'>
+                                <ArrowCircleDown size={24}></ArrowCircleDown>
+                                Saída
+                            </TransactionTypeButton>
+                        </TransactionType>
 
                         <button
                         type='submit'>
