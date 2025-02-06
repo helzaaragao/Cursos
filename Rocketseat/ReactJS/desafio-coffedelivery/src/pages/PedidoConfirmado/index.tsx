57eada541d08308bfb5bc1091bc41cb198c6830d
Helza Aragão <helza.batista.aragao@gmail.com>
2025-02-06 15:21:34 -0300

    Página Sucesso: Fazendo design

--- Rocketseat/ReactJS/desafio-coffedelivery/src/pages/PedidoConfirmado/index.tsx
+++ Rocketseat/ReactJS/desafio-coffedelivery/src/pages/PedidoConfirmado/index.tsx
@@ -1,5 +1,5 @@
-import { MapPin, Money, Timer } from "@phosphor-icons/react";
-import { Details, Heading, Ilustration, PedidoConfirmadoContainer } from "./style";
+import { CurrencyDollar, MapPin, Timer } from "@phosphor-icons/react";
+import { Details, Heading, Ilustration, Info, PedidoConfirmadoContainer } from "./style";
 import { useParams } from "react-router-dom";
 import { useCart } from "../../hooks/useCart";
 import ilustration from '../../assets/Illustration.svg'
@@ -10,8 +10,8 @@ export function PedidoConfirmado(){
     const {orderId} = useParams()
     const orderInfo = orders.find((order) => order.id === Number(orderId))
     const paymentMethod = { 
-        credit: 'Cartão de crédito', 
-        debit: 'Cartão de débito', 
+        credit: 'Cartão de Crédito', 
+        debit: 'Cartão de Débito', 
         cash: 'Dinheiro'
     }
 
@@ -29,25 +29,41 @@ export function PedidoConfirmado(){
                     <p>Agora é só aguardar que logo o café chegará até você</p>
                 </Heading>
              
-                <div>
-                    <MapPin
-                        color={theme.white}
-                    ></MapPin>
+                <Info>
                     <div>
-                            <span>Entregue em <strong>{orderInfo.street}, {orderInfo.number} </strong></span>
+                        <MapPin
+                            color={theme['base-white']}
+                            style={{ backgroundColor: theme['purple'] }}
+                            weight="fill"
+                            size={32}
+                        ></MapPin>
+                        <div>
+                                <span>Entregue em <strong>{orderInfo.street}, {orderInfo.number} </strong>
+                                </span>
+                                
+                                <span> {orderInfo.neighborhood} - {orderInfo.city}, {orderInfo.state}</span>
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <Timer></Timer>
-                    <span>
-                        Previsão de entrega
-                        <strong>20 min - 30 min</strong>
-                    </span>
-                </div>
-                <div>
-                    <Money></Money>
-                    <span>Pagamento na entrega <strong>{paymentMethod[orderInfo.paymentMethod]}</strong></span>
-                </div>
+                    <div>
+                        <Timer  color={theme['base-white']}
+                            style={{ backgroundColor: theme['yellow'] }}
+                            weight="fill"
+                            size={32}
+
+                        ></Timer>
+                        <span>
+                            Previsão de entrega<strong> 40 min - 60 min </strong>
+                        </span>
+                    </div>
+                    <div>
+                        <CurrencyDollar color={theme['base-white']}
+                         style={{ backgroundColor: theme['yellow-dark'] }}
+                         size={32}
+                        ></CurrencyDollar>
+                        <span>Pagamento na entrega <strong>{paymentMethod[orderInfo.paymentMethod]}</strong></span>
+                    </div>
+                </Info>
+              
             </Details>
             <Ilustration>
                 <img src={ilustration} alt="" />
--- Rocketseat/ReactJS/desafio-coffedelivery/src/pages/PedidoConfirmado/style.ts
+++ Rocketseat/ReactJS/desafio-coffedelivery/src/pages/PedidoConfirmado/style.ts
@@ -6,6 +6,8 @@ export const PedidoConfirmadoContainer = styled.main`
     align-items: center;
     justify-content: center;
     padding: 1rem 8rem 1rem 8rem;
+    gap: 10rem;
+    margin-top: 3rem;
 `
 export const Details = styled.section``
 export const Heading = styled.div`
@@ -15,5 +17,51 @@ export const Heading = styled.div`
         font-family: 'Baloo 2', sans-serif;
         font-weight: bolder;
     }
+
+    p{ 
+        margin-top: 0.6rem; 
+    }
+`
+export const Info = styled.section`
+
+        width: 35rem;
+        height: 15rem;
+        margin-top: 2rem;
+        position: relative;
+        border-radius: 0.375rem 2rem;
+        background-origin: border-box;
+        background-color: white; 
+
+        display: flex;
+        flex-wrap: wrap;
+        flex-direction: column;
+        justify-content: center;
+        padding-left: 2.5rem;
+        padding-right: 2.5rem;
+
+        &::before {
+            content: '';
+            position: absolute;
+            top: -1px;
+            left: -1px;
+            right: -1px;
+            bottom: -1px;
+            z-index: -1;
+            border-radius: 0.375rem 2rem;
+            background: linear-gradient(to bottom right, ${({ theme }) => theme['yellow']}, ${({ theme }) => theme['purple']});
+        }
+
+        > div{
+            display:flex;
+            align-items: center;
+            gap: 0.6rem;
+            margin-bottom: 1rem;
+                    > svg {
+                       padding: 8px;
+                       border-radius: 999px;
+                    }
+        }
+
+        
 `
 export const Ilustration = styled.section``
